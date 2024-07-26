c2c528a4c81a1c3ed5169255c77813b2118f9baf
monir178 <monir.mzs17@gmail.com>
2024-07-26 18:46:29 +0600

    sidebar added

--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -21,7 +21,7 @@ const Sidebar = () => {
               className={cn(
                 "flex gap-4 items-center p-4 rounded-lg justify-start",
                 {
-                  "bg-blue-1": isActive,
+                  "bg-blue-500": isActive,
                 }
               )}>
               {link.label}
