fbf4946dc746bec4ec7e58084264a244a5397094
monir178 <monir.mzs17@gmail.com>
2024-08-07 23:29:46 +0600

    upcoming meeting implemented

--- constants/index.ts
+++ constants/index.ts
@@ -25,3 +25,12 @@ export const sidebarLinks = [
         icon: '/icons/add-personal.svg',
     },
 ]
+
+
+export const avatarImages = [
+    '/images/avatar-1.jpeg',
+    '/images/avatar-2.jpeg',
+    '/images/avatar-3.jpeg',
+    '/images/avatar-4.jpeg',
+    '/images/avatar-5.jpeg',
+]
